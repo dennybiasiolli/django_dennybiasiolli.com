6dabb2df9eff9bd625dc1790b759d7fa21d5e296
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-06-05 22:29:59 +0200

    blog: added original template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def blog_home(request):
+    return render(request, 'blog/base.html', {})
+
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
