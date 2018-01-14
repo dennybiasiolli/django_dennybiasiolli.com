8a0379e41570878efb60ea1564996f7d5f0e25bd
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-01-14 20:47:54 +0100

    WIP

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,9 @@ from .models import Post, Work
 
 
 def blog_home(request):
-    return render(request, 'blog/base.html', {})
+    works = Work.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/base.html', {'works': works})
 
 
 def post_list(request):
